217bd355a8da3b8b49df2fe3c0657e8e2398a94a
Anton Sergeyev <sergeyev.anton.s@gmail.com>
2018-02-11 19:14:03 -0800

    RMICallInfo is now automatically serializable

--- rmi/RMICallInfo.java
+++ rmi/RMICallInfo.java
@@ -1,8 +1,16 @@
 package rmi;
 
-class RMICallInfo {
+import java.io.Serializable;
+
+class RMICallInfo implements Serializable {
     public String className;
     public String methodName;
     public Object[] args;
+
+    RMICallInfo(String className, String methodName, Object[] args) {
+        this.className = className;
+        this.methodName = methodName;
+        this.args = args;
+    }
 }
 
